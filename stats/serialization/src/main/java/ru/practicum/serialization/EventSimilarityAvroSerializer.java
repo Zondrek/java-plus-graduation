7440d168d9b8240b49b1a23898d62e6a1a4a0d4c
alexguru <zong.an@yandex.ru>
2026-04-04 23:22:21 +0300

    Этап 3

---
+++ stats/serialization/src/main/java/ru/practicum/serialization/EventSimilarityAvroSerializer.java
@@ -0,0 +1,28 @@
+package ru.practicum.serialization;
+
+import org.apache.avro.io.BinaryEncoder;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.specific.SpecificDatumWriter;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Serializer;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class EventSimilarityAvroSerializer implements Serializer<SpecificRecordBase> {
+    @Override
+    public byte[] serialize(String topic, SpecificRecordBase data) {
+        if (data == null) return null;
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            BinaryEncoder encoder = EncoderFactory.get().binaryEncoder(out, null);
+            DatumWriter<SpecificRecordBase> writer = new SpecificDatumWriter<>(data.getSchema());
+            writer.write(data, encoder);
+            encoder.flush();
+            return out.toByteArray();
+        } catch (IOException e) {
+            throw new SerializationException("Error serializing Avro message", e);
+        }
+    }
+}
